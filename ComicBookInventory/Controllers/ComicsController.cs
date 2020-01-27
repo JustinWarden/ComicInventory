f697e9f63b0c3b6047cefefffe6eab040c418eb3
Newforce39 <justin.warden@outlook.com>
2020-01-27 14:08:47 -0500

    added comic views and controller

---
+++ ComicBookInventory/Controllers/ComicsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ComicBookInventory.Data;
+using ComicBookInventory.Models;
+
+namespace ComicBookInventory.Controllers
+{
+    public class ComicsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ComicsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comics
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Comics.Include(c => c.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Comics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comic = await _context.Comics
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comic == null)
+            {
+                return NotFound();
+            }
+
+            return View(comic);
+        }
+
+        // GET: Comics/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Comics/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,Title,Publisher,Year,VolumeNumber,Price,Notes,ComicImage")] Comic comic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comic);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", comic.UserId);
+            return View(comic);
+        }
+
+        // GET: Comics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comic = await _context.Comics.FindAsync(id);
+            if (comic == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", comic.UserId);
+            return View(comic);
+        }
+
+        // POST: Comics/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,Title,Publisher,Year,VolumeNumber,Price,Notes,ComicImage")] Comic comic)
+        {
+            if (id != comic.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comic);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComicExists(comic.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", comic.UserId);
+            return View(comic);
+        }
+
+        // GET: Comics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comic = await _context.Comics
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comic == null)
+            {
+                return NotFound();
+            }
+
+            return View(comic);
+        }
+
+        // POST: Comics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comic = await _context.Comics.FindAsync(id);
+            _context.Comics.Remove(comic);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComicExists(int id)
+        {
+            return _context.Comics.Any(e => e.Id == id);
+        }
+    }
+}
